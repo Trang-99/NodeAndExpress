ff9775fd822de151fed7574f1830b48f2c635cdd
Trang-99 <59683509+Trang-99@users.noreply.github.com>
2020-02-25 22:38:05 +0700

    Initial commit

---
+++ myserver.js
@@ -0,0 +1,15 @@
+var express=require('express');
+var app=express();
+app.set('view engine','jade');
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res)
+{
+    res.render('index',{title:'BinhDQ title',message:'Welcome BinhDQ',"book": {"name": "Hello", "price": 12.99}})
+});
+
+app.get('/test',function(req,res)
+{
+    res.send('Hello test!');
+});
+var server=app.listen(port,function() {});
